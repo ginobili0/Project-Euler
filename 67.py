a5cd1ea6d1f981b4a8b72f1b39c4f2199da0c586
Teja K <yaswateja@gmail.com>
2016-01-04 16:31:31 -0500

    added 18 and 67

---
+++ 67.py
@@ -0,0 +1,12 @@
+f = open('prob-67.txt')
+nums = []
+for line in f:
+    n = [int(i) for i in line.rstrip('\n').split(' ')]
+    nums.append(n)
+
+res = nums.pop()
+while nums:
+    temp = nums.pop()
+    res = [temp[i] + max(res[i], res[i + 1]) for i in xrange(len(temp))]
+
+print res
